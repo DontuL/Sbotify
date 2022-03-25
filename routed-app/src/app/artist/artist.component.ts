24d3b8adc1d160805a7ed04e17cf2bd09d2fc4ba
DontuL <dontu.lilian@iisgalvanimi.edu.it>
2022-03-25 10:55:49 +0000

    search-get artist

--- routed-app/src/app/artist/artist.component.ts
+++ routed-app/src/app/artist/artist.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { SpotifyService } from '../spotify.service';
+import { Observable } from 'rxjs';
+import {Location} from '@angular/common'
+
 
 @Component({
   selector: 'app-artist',
@@ -6,10 +11,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./artist.component.css']
 })
 export class ArtistComponent implements OnInit {
+  routeObs: Observable<ParamMap>; 
 
-  constructor() { }
+  artist : any; //Qui salverò la traccia selezionata
+  spotifyServiceObs: Observable<Object>;
+  
+  constructor(  
+    private route: ActivatedRoute, 
+    private router: Router, 
+    private service: SpotifyService,
+    private location: Location) { }
 
   ngOnInit(): void {
+    this.routeObs = this.route.paramMap;
+    this.routeObs.subscribe(this.getRouterParam);
   }
-
+  getRouterParam = (params: ParamMap) =>
+  {
+    let artistId = params.get('id'); //Ottengo l'id dai parametri
+    console.log (artistId); //Stampo su console
+    //spotifyServiceObs va dichiarato
+    this.spotifyServiceObs = this.service.getArtist(artistId) ;
+    this.spotifyServiceObs.subscribe((data)=>this.artist = data)
+  }
 }
--- routed-app/src/app/spotify.service.ts
+++ routed-app/src/app/spotify.service.ts
@@ -17,13 +17,30 @@ export class SpotifyService {
     const headers = new HttpHeaders({Authorization: environment.oauthToken});
 ;
 
+
     let obsTracks = this.http.get(url, { headers });
     return obsTracks;
     
  //Ritorno un observable ai componenti che richiedono il servizio
   }
+  searchArtist(query: string) {
+    const url = `https://api.spotify.com/v1/search?q=${query}&type=artist`;
+    const headers = new HttpHeaders({Authorization: environment.oauthToken});
+  ;
+  let obsArtist = this.http.get(url, { headers });
+  return obsArtist;
+  
+//Ritorno un observable ai componenti che richiedono il servizio
+}
   getTrack(id: string) {
     const url = `https://api.spotify.com/v1/tracks/${id}`;
+    const headers = new HttpHeaders({Authorization: environment.oauthToken});
+;
+    
+    return this.http.get(url, { headers });
+  }
+  getArtist(id: string) {
+    const url = `https://api.spotify.com/v1/artists/${id}`;
     const headers = new HttpHeaders({Authorization: environment.oauthToken});
 ;
     
