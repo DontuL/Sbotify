910ac839ae0758c8f681bed09166786fc580154e
DontuL <dontu.lilian@iisgalvanimi.edu.it>
2022-03-30 11:54:39 +0000

    artist done

--- routed-app/src/environments/environment.ts
+++ routed-app/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  oauthToken: "Bearer BQBTJKVjhXhAh9-1dRQJ5AmB8hGde0lHLRfBLkWN7ZYH2vzZaDL9iI-p5ZSs5Wz3fCQePe5ZGI0JvhuFw7TKHM4HINU_zpa-FqbdBL6nDdDthgDAsPq_X53qPjZ9hxwDMNLxB6EpRGBiAlV5rfS6QK4-J9J5ynvqVZw"
+  oauthToken: "Bearer BQCGdSYwD0s05TPk9sDTUtRhXehcQA88ocIMrNkfMc_RcBG2ngj93iD3J5tPxe8i5UttyHs16e6cCFOjvu7D0gRDBCBRaR4cp_v-HfwmhwjTqk4BpTBCyCTle931NAyFbURB8j7CkIrGUPepYcVUm-SAfafD96hgcnM"
 };
